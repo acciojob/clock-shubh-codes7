320e1664e26111c196fa81290b695c0f227be97a
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-17 20:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let ptag = document.querySelector("#timer")
+
+let currentTime = new Date()
+
+setInterval(
+	function(){
+		currentTime = new Date()
+		ptag.innerText = currentTime.toLocaleString();
+		
+	}, 1000)
+
